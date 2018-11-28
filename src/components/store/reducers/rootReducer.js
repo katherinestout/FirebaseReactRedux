dc0252f83f6498e9e053f15792ffdb745c0ebad6
katherinestout <kjs14@hood.edu>
2018-11-28 14:04:35 -0500

    created store

---
+++ src/components/store/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+//combining both reducers
+import authReducer from './authReducer';
+import projectReducer from './projectReducer';
+import {combineReducers} from 'redux';
+
+//auth and project updates information on auth property in state
+const rootReducer = combineReducers({
+    auth: authReducer,
+    project: projectReducer
+})
+
+export default rootReducer;
