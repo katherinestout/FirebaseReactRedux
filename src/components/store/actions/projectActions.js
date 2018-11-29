94d95234b2784bf957b994f51057a405e3a6ef8f
katherinestout <kjs14@hood.edu>
2018-11-29 13:49:44 -0500

    adding reducers and actions

--- src/components/store/actions/projectActions.js
+++ src/components/store/actions/projectActions.js
@@ -1,6 +1,22 @@
 export const createProject = (project) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => {
 
+        //make a reference to firestore DB
+        const firestore = getFirestore();
+        //making it possible to add project objects
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Kat',
+            authorLastName: 'Stout',
+            //generates id
+            authorId: 12345,
+            createdAt: new Date()
+            //.then is the callback when the task above is complete
+        }).then(() => {
+            dispatch({type: 'CREATE_PROJECT', project});
+        }).catch((err) => {
+            dispatch({type: 'CREATE_PROJECT_ERROR', err });
+        })
         //make asynch call to DB
         dispatch({type: 'CREATE_PROJECT', project});
 
--- src/components/store/reducers/projectReducer.js
+++ src/components/store/reducers/projectReducer.js
@@ -13,9 +13,16 @@ const projectReducer = (state = initState, action) =>
     //check action type
     switch(action.type) {
         case 'CREATE_PROJECT':
-        console.log('created project', action.project)
+        console.log('created project', action.project);
+        return state;
+        
+        case 'CREATE_PROJECT_ERROR':
+        console.log('create project error', action.error);
+        return state;
+
+        default:
+        return state;
     }
-    return state
 }
 
 export default projectReducer;
