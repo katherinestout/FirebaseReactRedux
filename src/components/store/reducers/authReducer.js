e6da50074bd64a1f5fe4c1dad0c28774c665a711
katherinestout <kjs14@hood.edu>
2018-11-29 15:30:47 -0500

    auth reducer

--- src/components/store/reducers/authReducer.js
+++ src/components/store/reducers/authReducer.js
@@ -1,7 +1,25 @@
-const initState = {}
+const initState = {
+    authError: null
+}
 
 const authReducer = (state = initState, action) =>{
-    return state
+    //check action type
+    switch(action.type){
+        case 'LOGIN_ERROR':
+        console.log('Login error!!')
+        return {
+            ...state,
+            authError: 'Login has failed'
+        }
+        case 'LOGIN_SUCESS':
+        console.log('Login success');
+        return {
+            ...state,
+            authError: null
+        }
+        default:
+        return state;
+    }
 }
 
 export default authReducer;
