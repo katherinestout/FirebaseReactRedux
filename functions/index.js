a9c2fb7ac8495d79b13c5845e9d9a64270198b79
katherinestout <kjs14@hood.edu>
2018-11-30 14:36:57 -0500

    cloud functions notifications

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+//createnotification is called when you do something new
+const createNotification = (notification => {
+    return admin.firestore().collection('notifications')
+    .add(notification)
+    .then(doc => console.log('notification added', doc));
+});
+
+//1.when a project is created
+exports.projectCreated = functions.firestore
+    .document('projects/{projectId}')
+    //references a specific document
+    .onCreate(doc => {
+        //whenever a new project is created it fires this function, which creates a new notification
+        const project = doc.data();
+        //notification object:
+        const notification = {
+            content: 'Added a new project',
+            user: `${project.authorFirstName} ${project.authorLastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+            //timestamp from server
+        };
+        return createNotification(notification);
+});
+
+//2.
