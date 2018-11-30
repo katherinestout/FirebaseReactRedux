c89205d6a6fa622df895af9c4fdfbebec95980ca
katherinestout <kjs14@hood.edu>
2018-11-29 19:43:40 -0500

    turnery operator

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,15 +5,21 @@ import SignedOutLinks from './SignedOutLinks';
 import {connect} from 'react-redux';
 //import { map } from '@firebase/util';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const {auth} = props;
+   // console.log(auth);
+
+   //turnery operator to show or hide links depending on uid matches
+   const links = auth.uid ? <SignedInLinks/> : <SignedOutLinks/>;
+
     return (
         <nav className="nav-wrapper grey darken-3">
         <div className="container">
         <Link to ='/' className="brand-logo">
-        MarioPlan
+        KatProjectPlan
         </Link>
-        <SignedInLinks/>
-        <SignedOutLinks/>
+        
+        {links}
 
         </div>
         
@@ -21,6 +27,8 @@ const Navbar = () => {
         
     )
 }
+
+//we can access this state in this property
 
 const mapStateToProps = (state) => {
     console.log(state);
