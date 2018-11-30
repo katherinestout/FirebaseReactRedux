57076053ea9af7bdca5af499d214f0237a0dbf51
katherinestout <kjs14@hood.edu>
2018-11-30 16:12:38 -0500

    fini

---
+++ dist/precache-manifest.1d452d331176f9e6c3fc0dd5e957225c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf127c2f35c444d802d2",
+    "url": "/static/js/main.bf127c2f.chunk.js"
+  },
+  {
+    "revision": "f7a75775aa108184223c",
+    "url": "/static/js/1.f7a75775.chunk.js"
+  },
+  {
+    "revision": "bf127c2f35c444d802d2",
+    "url": "/static/css/main.3d9700f4.chunk.css"
+  },
+  {
+    "revision": "79166eb44ceeefc0c1a484124ca0593b",
+    "url": "/index.html"
+  }
+];
